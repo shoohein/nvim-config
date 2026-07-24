7a14466ebc5e658e203a75ecf996c7d7a0e1b95f
Shuhei Nitta <shuheinitta.dev@gmail.com>
2026-07-24 15:17:54 +0900

    feat(format): add Python formatter support

--- lua/config/fmt/init.lua
+++ lua/config/fmt/init.lua
@@ -2,6 +2,7 @@ require("conform").setup({
   formatters_by_ft = {
     go = require("config.fmt.go"),
     lua = require("config.fmt.lua"),
+    python = require("config.fmt.python"),
   },
   format_on_save = {
     timeout_ms = 1000,
