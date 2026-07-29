315564b6601259c0cd9b3be8f9758c4a1355cb21
Shuhei Nitta <shuheinitta.dev@gmail.com>
2026-07-29 13:47:44 +0900

    feat(keymap): add terminal and window navigation

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,2 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("i", "jj", "<Cmd>stopinsert<CR>", { desc = "Exit insert mode" })
+
+vim.keymap.set("n", "<leader>t", "<Cmd>botright vsplit | terminal<CR>", { desc = "Open terminal on the right" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Move to left window" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Move to lower window" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Move to upper window" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Move to right window" })
