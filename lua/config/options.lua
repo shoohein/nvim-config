3470e905f6ce7d2e0ce65308eedc70950aa42c54
Shuhei Nitta <shuheinitta.dev@gmail.com>
2026-08-05 13:59:18 +0900

    fix(editor): use dict literal for Python indent config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,5 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 
-vim.g.python_indent = vim.g.python_indent or {}
-vim.g.python_indent.open_paren = "shiftwidth()" -- indent 4 chars after '('
+vim.g.python_indent = {
+  open_paren = "shiftwidth()",
+  closed_paren_align_last_line = false,
+}
