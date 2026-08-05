6f5bab4ec17477c16b641c6df5e1d8bb82a19ccb
Shuhei Nitta <shuheinitta.dev@gmail.com>
2026-08-05 13:35:55 +0900

    chore(editor): configure Python indentation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,6 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+
+vim.g.python_indent = vim.g.python_indent or {}
+vim.g.python_indent.open_paren = "shiftwidth()" -- indent 4 chars after '('
