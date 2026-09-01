b1ab85a55164d94642e5b2c213d64afdcd4c0e73
Shuhei Nitta <shuheinitta.dev@gmail.com>
2026-09-01 14:13:51 +0900

    feat(treesitter): install parsers for supported languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,16 @@ return {
   cmd = { "TSInstall", "TSUpdate" },
   event = { "BufReadPost", "BufNewFile" },
   opts = {
-    ensure_installed = { "markdown", "markdown_inline" },
+    ensure_installed = {
+      "markdown",
+      "markdown_inline",
+      "go",
+      "python",
+      "c",
+      "cpp",
+      "bash",
+      "lua",
+    },
     highlight = { enable = true },
     indent = { enable = true },
   },
